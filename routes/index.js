ce85c7d73bdfddf3a8e95d6f807db503c39d0e4d
Denise Yu <yu.denise.d@gmail.com>
2020-02-09 15:43:23 -0500

    did something dumb

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var redis
 
 // heroku readiness
 if (process.env.REDISTOGO_URL) {
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+  redis = require("redis").createClient(rtg.port, rtg.hostname);
   redis.auth(rtg.auth.split(":")[1]);
 } else {
-  var redis = require("redis");
+  redis = require("redis");
 }
 
 var client = redis.createClient();
