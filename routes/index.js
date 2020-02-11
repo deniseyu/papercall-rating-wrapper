8def34eeb2c4173a81012e3cc5aeb5fdd3e2ae88
Denise Yu <yu.denise.d@gmail.com>
2020-02-11 10:10:04 -0500

    add memoization for redis.keys('*')

--- routes/index.js
+++ routes/index.js
@@ -7,54 +7,63 @@ var async = require('async');
 var basicAuth = passport.authenticate('local', { failureRedirect: '/login'})
 var loggedIn = require('connect-ensure-login').ensureLoggedIn
 
+// running redis.keys is risky for memory, and set of talks unlikely to change
+var memoizedKeys // so let's cache it in application memory
+
 router.get('/', loggedIn(), function(req, res) {
-  // todo: only fetch all keys once, then memoize - this is bad for prod
-  redis.keys("*", function(err, keys) {
-    var talkKeys = keys.filter(key => !key.includes(':')) // for now ':' denotes a rating
-    var myAssignments
-
-    function getTalks(keys, cb) {
-      redis.mget(keys, function(err, talks) {
-        var talksResult = { all: [], pending: [], mine: [] }
-        if (talks == null || talks.length == 0) { return cb(null, talksResult) }
-        var talksCB = talks.map(t => JSON.parse(t)).map((t, ind) => {
-          t.id = keys[ind]
-          return t
-        })
-        talksResult.all = talksCB
-        cb(null, talksResult)
-      })
+  function getAllKeys(matcher, cb) {
+    if (memoizedKeys) {
+      console.log('using keys from mem')
+      return cb(null, memoizedKeys)
     }
+    redis.keys(matcher, function(err, results) {
+      console.log('pulling from redis')
+      memoizedKeys = results.filter(key => !key.includes(':')) // for now ':' denotes a rating
+      cb(null, memoizedKeys)
+    })
+  }
 
-    function isolateAssigned(talksResult, cb) {
-      redis.lrange(`assign:${req.user.username}`, 0, 1000, function(err, myTalkIDs) {
-        talksResult.mine = talksResult.all.filter(talk => myTalkIDs.includes(talk.id))
-        talksResult.notMine = talksResult.all.filter(talk => !myTalkIDs.includes(talk.id))
-        cb(null, talksResult)
+  function getTalks(keys, cb) {
+    redis.mget(keys, function(err, talks) {
+      var talksResult = { all: [], pending: [], mine: [] }
+      if (talks == null || talks.length == 0) { return cb(null, talksResult) }
+      var talksCB = talks.map(t => JSON.parse(t)).map((t, ind) => {
+        t.id = keys[ind]
+        return t
       })
-    }
+      talksResult.all = talksCB
+      cb(null, talksResult)
+    })
+  }
 
-    function pendingReview(talksResult, cb) {
-      redis.lrange(`complete:${req.user.username}`, 0, 1000, function(err, reply) {
-        talksResult.pending = talksResult.mine.filter(t => !reply.includes(t.id))
-        talksResult.completed = talksResult.mine.filter(t => reply.includes(t.id))
-        cb(null, talksResult)
-      })
-    }
+  function isolateAssigned(talksResult, cb) {
+    redis.lrange(`assign:${req.user.username}`, 0, 1000, function(err, myTalkIDs) {
+      talksResult.mine = talksResult.all.filter(talk => myTalkIDs.includes(talk.id))
+      talksResult.notMine = talksResult.all.filter(talk => !myTalkIDs.includes(talk.id))
+      cb(null, talksResult)
+    })
+  }
 
-    var processTalks = async.compose(pendingReview, isolateAssigned, getTalks)
+  function pendingReview(talksResult, cb) {
+    redis.lrange(`complete:${req.user.username}`, 0, 1000, function(err, reply) {
+      talksResult.pending = talksResult.mine.filter(t => !reply.includes(t.id))
+      talksResult.completed = talksResult.mine.filter(t => reply.includes(t.id))
+      cb(null, talksResult)
+    })
+  }
 
-    processTalks(talkKeys, function(err, results) {
-      res.render('index', {
-        pending: results.pending,
-        mine: results.mine,
-        completed: results.completed,
-        others: results.notMine,
-        user: req.user
-      })
+  var processTalks = async.compose(pendingReview, isolateAssigned, getTalks, getAllKeys)
+
+  processTalks("*", function(err, results) {
+    res.render('index', {
+      pending: results.pending,
+      mine: results.mine,
+      completed: results.completed,
+      others: results.notMine,
+      user: req.user
     })
   })
-});
+})
 
 router.post('/assign/:user/:talkID', function(req, res) {
   var key = `assign:${req.params.user}`
